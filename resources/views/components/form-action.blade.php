41dd4889756837721d00fd805626fb2fa8beaefc
Ronan Campos <ronannc1@yahoo.com.br>
2022-08-21 21:27:30 -0300

    historico da movimentacao das species

---
+++ resources/views/components/form-action.blade.php
@@ -0,0 +1,4 @@
+<div class="form-actions right">
+    <a href="{{ URL::previous() }}" class="btn btn-default float-right">Cancelar</a>
+    <button type="submit" class="btn btn-primary">Salvar</button>
+</div>
